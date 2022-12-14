88bf7aa82bd0275e28d07995c46f83d27e8e2f98
smckenzie78 <109680885+smckenzie78@users.noreply.github.com>
2022-12-14 00:49:38 -0500

    Update script.js

--- Restaurant Website/script.js
+++ Restaurant Website/script.js
@@ -1,3 +1,15 @@
 function notImplemented(){
     alert("Sorry, this feature has not been implemented :(\nThis website exists mainly to show off the UI");
 }
+
+function togglemenu(){
+    let links = document.getElementById("links");
+    links.style.marginLeft = "20%";
+    links.style.transition = "0.4s";
+}
+
+function closemenu(){
+    let links = document.getElementById("links");
+    links.style.marginLeft = "100%";
+    links.style.transition = "0.6s";
+}
